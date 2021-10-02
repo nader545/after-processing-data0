cb01e3afcbdc8fe69ad01d32c8a60c47c76da200
Nader <nadrmhmd057@gmail.com>
2021-10-02 03:59:19 +0200

    first

---
+++ TACK_1/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace TACK_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt_inPath = new System.Windows.Forms.TextBox();
+            this.txt_OutPath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(180, 190);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 51);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt_inPath
+            // 
+            this.txt_inPath.Location = new System.Drawing.Point(105, 39);
+            this.txt_inPath.Name = "txt_inPath";
+            this.txt_inPath.Size = new System.Drawing.Size(325, 33);
+            this.txt_inPath.TabIndex = 2;
+            // 
+            // txt_OutPath
+            // 
+            this.txt_OutPath.Location = new System.Drawing.Point(105, 104);
+            this.txt_OutPath.Name = "txt_OutPath";
+            this.txt_OutPath.Size = new System.Drawing.Size(325, 33);
+            this.txt_OutPath.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 328);
+            this.Controls.Add(this.txt_OutPath);
+            this.Controls.Add(this.txt_inPath);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt_inPath;
+        private System.Windows.Forms.TextBox txt_OutPath;
+    }
+}
+
---
+++ TACK_1/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TACK_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string inputFilePath = txt_inPath.Text;
+            string OutputFilePath = txt_OutPath.Text;  //Please type the path only without the file name
+            ParseData(inputFilePath, OutputFilePath);
+        }
+
+        public static void ParseData(string inputFilePath, string OutputFilePath)
+        {     
+            string date_file_name = inputFilePath.Substring(inputFilePath.Length - 13, 10);  //In this line, the date has been omitted from the file path;
+            string Path = OutputFilePath + date_file_name + ".ur";      //In this line, specify the path to save the entire file with the date;
+            if (File.Exists(inputFilePath))                // we want to make sure that the file exists in this path;
+            {
+                StreamReader sr = new StreamReader(inputFilePath, Encoding.Default);    
+                var first_line = sr.ReadLine().Split(',').ToList();                    
+                string date_first_line = first_line[0].ToString().Substring(5);        //the date in the first line;
+                if (date_first_line != date_file_name)       //Make sure that the date in the file name matches the date on the first line inside the file;
+                {
+                    MessageBox.Show("invalid file!! : file name don’t match with date in first line");
+                    return;
+                }                               
+                string Line;             
+                int count = 0;
+                List<string> file = new List<string>();         
+                while ((Line = sr.ReadLine()) != null)         //Read all lines from the file and put them in a list ;
+                {
+                    file.Add(Line);
+                    count++;
+                }
+                string count_first_line = first_line[1].ToString().Substring(6);
+                if (Convert.ToInt32(count_first_line) != count)                    //Verify that the number of burglaries on the first line is correct;
+                {
+                    MessageBox.Show("invalid file!! : count on first line don’t match with lines count ");
+                    return;
+                }
+                using (StreamWriter sw = new StreamWriter(Path))
+                {
+                    sw.WriteLine(first_line[0].ToString() + "," + first_line[1].ToString() + "\n" + "{");  //Write the first line in the processing file;
+                    for (int i = 0; i < file.Count; i++)
+                    {
+                          //Separate all the lines into two halves, between [] in one part, and before them in one part;
+                        string[] All = file[i].ToString().Split(new string[] { "[", "]" }, StringSplitOptions.None);
+                        string[] first_half = All[0].Split(new char[] { ',' });
+                        sw.WriteLine("    [");
+                        sw.WriteLine("        age:" + first_half[0].ToString() + "," + "\n" + "        country:" + first_half[1].ToString() + "," + "\n" + "        Name:" + first_half[2].ToString()); //Writing the first half;
+                        if (All[1].Contains(","))     //Find out if there is between [] more than one element;
+                        {
+                            string[] last_half = All[1].Split(new char[] { ',' });
+                            sw.WriteLine("        Date:[");
+                            for (int x = 0; x < last_half.Length; x++)
+                            {
+                                sw.WriteLine("             " + last_half[x] + ","); //Write more than one element;                      
+                            }
+                            sw.WriteLine("        ]");
+                            sw.WriteLine("    ]");
+                        }
+                        else
+                        {
+                            sw.WriteLine("        Date:[");
+                            sw.WriteLine("            "+All[1]);
+                            sw.WriteLine("        ]");
+                            sw.WriteLine("    ],");
+                        }
+                    }
+                }
+            }
+            MessageBox.Show(" The data has been processed successfully ");
+        }
+    }
+}
+ 
